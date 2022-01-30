e0dc31f45c3b737f75c4b215d32e4312b012b651
Ruben Nijhuis <contact@rubennijhuis.com>
2022-01-30 11:15:01 +0100

    🎉  feat: added arg validation [WIP]

---
+++ src/utils/initiate_data.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   initiate_data.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/28 08:49:44 by rubennijhui   #+#    #+#                 */
+/*   Updated: 2022/01/28 08:52:46 by rubennijhui   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/philo.h"
+
+void initiate_data(t_program_data *pd, char **argv)
+{
+	int amount_philo = ft_atoi(argv[1]);
+	int time_to_die = ft_atoi(argv[2]);
+	int time_to_eat = ft_atoi(argv[3]);
+	int time_to_sleep = ft_atoi(argv[4]);
+
+	pd->amount_philo = amount_philo;
+	pd->time_to_die = time_to_die;
+	pd->time_to_eat = time_to_eat;
+	pd->time_to_sleep = time_to_sleep;
+
+	pd->forks = ft_calloc(pd->amount_philo, sizeof(int));
+}
---
+++ src/utils/validate_arguments.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   validate_arguments.c                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/27 20:03:05 by rubennijhui   #+#    #+#                 */
+/*   Updated: 2022/01/27 20:03:47 by rubennijhui   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+int validate_arguments(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	return (1);
+}
