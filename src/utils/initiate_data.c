796b8356c352a580a2dd5279f327197803121ef0
Ruben Nijhuis <contact@rubennijhuis.com>
2022-03-03 14:07:51 +0100

    ⚙️  refact: data initialization

--- src/utils/initiate_data.c
+++ src/utils/initiate_data.c
@@ -6,21 +6,32 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/28 08:49:44 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2022/02/28 13:51:09 by rnijhuis      ########   odam.nl         */
+/*   Updated: 2022/03/03 10:17:00 by rubennijhui   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/philo.h"
 
-void initiate_data(t_program_data *pd, char **argv)
+void	initiate_data(t_program_data *pd, char **argv)
 {
-	int amount_philo = ft_atoi(argv[1]);
-	int time_to_die = ft_atoi(argv[2]);
-	int time_to_eat = ft_atoi(argv[3]);
-	int time_to_sleep = ft_atoi(argv[4]);
+	struct timeval	tv;
 
-	pd->amount_philo = amount_philo;
-	pd->time_to_die = time_to_die;
-	pd->time_to_eat = time_to_eat;
-	pd->time_to_sleep = time_to_sleep;
+	pd->tv = &tv;
+	pd->amount_philo = ft_atoi(argv[1]);
+	pd->time_to_die = ft_atoi(argv[2]);
+	pd->time_to_eat = ft_atoi(argv[3]);
+	pd->time_to_sleep = ft_atoi(argv[4]);
+}
+
+void	initiate_table(t_program_data *pd)
+{
+	int	i;
+
+	i = 0;
+	while (i < pd->amount_philo)
+	{
+		make_fork(pd, i);
+		make_philo_thread(pd, i);
+		i++;
+	}
 }
--- src/utils/validate_arguments.c
+++ src/utils/validate_arguments.c
@@ -6,11 +6,11 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/27 20:03:05 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2022/01/27 20:03:47 by rubennijhui   ########   odam.nl         */
+/*   Updated: 2022/03/02 15:25:32 by rubennijhui   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int validate_arguments(int argc, char **argv)
+int	validate_arguments(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
