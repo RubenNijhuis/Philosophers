271afd4845bde7b8904b2fff7c6737321630fbc4
RubenNijhuis <contact@rubennijhuis.com>
2022-03-11 16:24:44 +0100

    cleanup

--- src/initiate.c
+++ src/initiate.c
@@ -6,7 +6,7 @@
 /*   By: rnijhuis <rnijhuis@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/10 13:25:22 by rnijhuis      #+#    #+#                 */
-/*   Updated: 2022/03/10 14:27:06 by rnijhuis      ########   odam.nl         */
+/*   Updated: 2022/03/11 13:29:35 by rnijhuis      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	initiate_data(t_program_data *pd, char **argv)
 	pd->time_to_die = ft_atoi(argv[2]);
 	pd->time_to_eat = ft_atoi(argv[3]);
 	pd->time_to_sleep = ft_atoi(argv[4]);
+	pd->start_time = gettime();
 	pthread_mutex_init(&pd->print_lock, NULL);
 	pthread_mutex_init(&pd->stop_sim_lock, NULL);
 }
